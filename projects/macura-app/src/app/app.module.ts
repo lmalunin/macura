7eed2bcce68ec05d88806dadc760fe5c1ddbceb6
lmalunin <lmalunin@gmail.com>
2021-06-24 23:37:33 +0300

    ngrx, ngrx-devtools installed

--- projects/macura-app/src/app/app.module.ts
+++ projects/macura-app/src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MacuraLibModule } from "../../../macura-lib/src/lib/macura-lib.module";
+import { StoreModule } from '@ngrx/store';
 
+import { MacuraLibModule } from "../../../macura-lib/src/lib/macura-lib.module";
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { metaReducers, reducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [
@@ -16,7 +21,11 @@ import { AppComponent } from './app.component';
         BrowserModule,
         BrowserAnimationsModule,
         /*Macura libs*/
-        MacuraLibModule
+        MacuraLibModule,
+        StoreModule.forRoot(reducers, {
+            metaReducers
+        }),
+        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
     ],
     providers: [],
     bootstrap: [AppComponent]
